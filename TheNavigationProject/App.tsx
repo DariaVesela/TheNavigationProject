c6ac387dba85c02868bfcdf84c872a022603fafa
Daria Vesela <daria1vesela@gmail.com>
2024-03-25 12:14:24 +0000

    This is a MOCK location - just work with it for now, final testing will be on phone. Need to figure out how to repeatedly pick up users location for continuous monitoring and accuracy

--- TheNavigationProject/App.tsx
+++ TheNavigationProject/App.tsx
@@ -3,12 +3,12 @@
 
 import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View, SafeAreaView, Pressable, PermissionsAndroid } from 'react-native';
-import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { Marker, PROVIDER_GOOGLE, Region } from "react-native-maps";
 import Geolocation from '@react-native-community/geolocation';
 
 function App(): JSX.Element {
 
-  const [region, setRegion] = useState<{ latitude: number, longitude: number, latitudeDelta: number, longitudeDelta: number } | null>(null);
+  const [region, setRegion] = useState<Region | null>(null);
 
   useEffect(() => {
         // Function to request location permission
@@ -26,32 +26,74 @@ function App(): JSX.Element {
             if (granted === PermissionsAndroid.RESULTS.GRANTED) {
               console.log('Location permission granted');
               // Access the device's location
-    Geolocation.getCurrentPosition(
-      position => {
-        const { latitude, longitude } = position.coords;
-        setRegion({
-          latitude,
-          longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        });
-      },
-      error => console.log("Error getting location:", error.message),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    );
-  } []);
+              Geolocation.getCurrentPosition(
+                (position:any) => {
+                  console.warn(position)
+                  
+                  const { latitude, longitude, accuracy } = position.coords;
+
+                  setRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: accuracy/1000, // THis needs fixing
+                    longitudeDelta: accuracy/1000,// THis needs fixing
+                  });
+                },
+                (error:any) => console.log("Error getting location:", error.message),
+                { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+              );
+            }
+          } catch (e:any){
+            console.warn(e)
+          }
+        }
+        requestLocationPermission()
+      },[]);
 
   return (
-    <View style={styles.wrapper}>
-      {region && (
-        <MapView
-          style={styles.map}
-          provider={PROVIDER_GOOGLE}
-          region={region}
-          showsUserLocation
-        />
-      )}
+    <SafeAreaView style={styles.wrapper}>
+
+<View style={styles.currentLocationBar}>
+  <Text>Location bar</Text>
+</View>    
+    {
+      region &&  <MapView style={styles.map}
+      provider={PROVIDER_GOOGLE}
+      region={region}
+      showsUserLocation
+    />  
+    
+    }
+    
+ 
+    {/* <Pressable style={styles.goButton}> */}
+    <View style={{flex:1}}>
+      <View style={{flex:1}}/>
+  
+        <Text style={{fontSize:60, color:'#000', textAlign: 'center'}}>Go!</Text>
+      
+     <View style={{flex:1}}/>
     </View>
+    {/* </Pressable> */}
+    {/* </View> */}
+
+
+    <View style={styles.goText}>
+
+    </View>
+
+
+    </SafeAreaView>
+    // <View style={styles.wrapper}>
+    //   {region && (
+    //     <MapView
+    //       style={styles.map}
+    //       provider={PROVIDER_GOOGLE}
+    //       region={region}
+    //       showsUserLocation
+    //     />
+    //   )}
+    // </View>
   );
 }
 
